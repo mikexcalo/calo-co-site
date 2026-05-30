b83b963c50904c4e0733abc64fe34b7b4d273861
mikexcalo <mikexcalo@gmail.com>
2026-05-29 20:57:11 -0400

    FounderMemo: centered single-column layout with portrait, quote, signature

--- components/FounderMemo.tsx
+++ components/FounderMemo.tsx
@@ -28,31 +28,27 @@ export default function FounderMemo() {
       ref={sectionRef}
       className={`${styles.section} ${inView ? styles.inView : ''}`}
     >
-      <div className={styles.split}>
-        <div className={styles.copyCol}>
-          <div className={styles.copyInner}>
-            <span className={styles.eyebrow}>A note from the founder</span>
-            <blockquote className={styles.quote}>
-              Every business deserves to look as serious as it actually is. <em>That&apos;s the whole job.</em>
-            </blockquote>
-            <div className={styles.signatureBlock}>
-              <div className={styles.signature} aria-hidden="true">Mike</div>
-              <p className={styles.signatureLabel}>
-                Mike Calo
-                <br />
-                <span className={styles.signatureRole}>
-                  Founder, CALO<span className="amp">&amp;</span>CO
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
+      <div className={styles.inner}>
+        <span className={styles.eyebrow}>A note from the founder</span>
         <div
           className={styles.portrait}
           style={{ backgroundImage: "url(/images/audiences/mike-artist-grade2.jpg)" }}
           role="img"
           aria-label="Mike Calo, Founder of CALO&CO"
         />
+        <blockquote className={styles.quote}>
+          Every business deserves to look as serious as it actually is. <em>That&apos;s the whole job.</em>
+        </blockquote>
+        <div className={styles.signatureBlock}>
+          <div className={styles.signature} aria-hidden="true">Mike</div>
+          <p className={styles.signatureLabel}>
+            Mike Calo
+            <br />
+            <span className={styles.signatureRole}>
+              Founder, CALO<span className="amp">&amp;</span>CO
+            </span>
+          </p>
+        </div>
       </div>
     </section>
   );
